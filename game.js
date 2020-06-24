26fef9b493b0f73ee90ceb78c609155223e7f341
Maylen Nunez <maylennunez@hotmail.com>
2020-06-23 21:35:47 -0400

    randomNumber

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-alert("hello")
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var buttonColours = ["red", "blue", "green", "yellow"];
+  var gamePattern = [];
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+}
