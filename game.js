bc33c5dc58123597ced7b29908e8375db6f5a137
Maylen Nunez <maylennunez@hotmail.com>
2020-06-27 21:23:38 -0400

    add sounds to button click

--- game.js
+++ game.js
@@ -1,18 +1,19 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = []; // At the top of the game.js file, create a new empty array with the name userClickedPattern.
 
-var userClickedPattern = []; //3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
 
+$(".btn").click(function() { // Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
 
-  $(".btn").click(function() { //1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+  var userChosenColour = $(this).attr("id"); // Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  console.log(this);
+  userClickedPattern.push(userChosenColour); // Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  console.log(userClickedPattern);
 
-  var userChosenColour = $(this).attr("id"); //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-   console.log(this);
-  userClickedPattern.push(userChosenColour); //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-
-   console.log(userClickedPattern);
-
+  playSound(userChosenColour); //  In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
 });
+
+
 
 function nextSequence() {
 
@@ -20,9 +21,13 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);      //1. Use jQuery to select the button with the same id as the randomChosenColour
-                                                                      //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-                                //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); // Use jQuery to select the button with the same id as the randomChosenColour
+  playSound(randomChosenColour);
+
+}
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
