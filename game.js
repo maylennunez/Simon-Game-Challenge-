ad7c5a070261b331e74d4a7832e16028756b17d7
Maylen Nunez <maylennunez@hotmail.com>
2020-07-03 22:25:45 -0400

    Star the game

--- game.js
+++ game.js
@@ -1,7 +1,23 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = []; // At the top of the game.js file, create a new empty array with the name userClickedPattern.
 
+var started = false;   //You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+
+var level = 0;        // Create a new variable called level and start at level 0.
+
+// Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+
+  if (!started) {
+    // The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+
+});
 
 $(".btn").click(function() { // Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
 
@@ -13,9 +29,11 @@ $(".btn").click(function() { // Use jQuery to detect when any of the buttons are
   playSound(userChosenColour); //  In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
 });
 
-
-
 function nextSequence() {
+  // Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+  // Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
