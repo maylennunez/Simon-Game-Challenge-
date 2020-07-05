924a37902f06fd9192d5af5bb8469d2817779fa7
Maylen Nunez <maylennunez@hotmail.com>
2020-07-04 22:02:56 -0400

    check the user answer

--- game.js
+++ game.js
@@ -3,9 +3,9 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = []; // At the top of the game.js file, create a new empty array with the name userClickedPattern.
 
-var started = false;   //You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false; //You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
 
-var level = 0;        // Create a new variable called level and start at level 0.
+var level = 0; // Create a new variable called level and start at level 0.
 
 // Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 $(document).keypress(function() {
@@ -25,15 +25,44 @@ $(".btn").click(function() { // Use jQuery to detect when any of the buttons are
   console.log(this);
   userClickedPattern.push(userChosenColour); // Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
   console.log(userClickedPattern);
-    animatePress(userChosenColour);
+  animatePress(userChosenColour);
   playSound(userChosenColour); //  In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
+
+  checkAnswer(userClickedPattern.length - 1); // Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+
+
 });
 
+
+function checkAnswer(currentLevel) { // Create a new function called checkAnswer(), it should take one input with the name currentLevel
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) { // Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) { // If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+
+      setTimeout(function() { // Call nextSequence() after a 1000 millisecond delay.
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    console.log("wrong");
+
+  }
+
+}
+
 function nextSequence() {
-  // Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
-  level++;
-  // Inside nextSequence(), update the h1 with this change in the value of level.
-  $("#level-title").text("Level " + level);
+
+  userClickedPattern = [];                                  // Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+
+  level++;                                               // Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  $("#level-title").text("Level " + level);                     // Inside nextSequence(), update the h1 with this change in the value of level.
+
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
@@ -50,12 +79,11 @@ function playSound(name) {
   audio.play();
 }
 
-function animatePress(currentColor){
-  // Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+function animatePress(currentColor) {
+  // add this pressed class to the button that gets clicked inside animatePress().
   $("#" + currentColor).addClass("pressed");
 
-  // use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
-  setTimeout(function () {
+    setTimeout(function() {                              // remove the pressed class after a 100 milliseconds.
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 
